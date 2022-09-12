correct the calculation of phase diff when doing PBA lens design. The |exp(ip1) - exp(ip2)|^2 is used

Meta_SCMT/PBA_utils/PBA_1D_lam.py
@@ -3,7 +3,8 @@
 '''
 import numpy as np
 import matplotlib.pyplot as plt
-from .PBA_model_1D_lam import PBA_model
+from .PBA_model_1D_lam import PBA_model, PBA_model_2_layer
+from ..SCMT_utils.SCMT_1D import plot_hs, plot_FFT_I, plot_If, gen_x_angle, loss_max_center, loss_max_range
 import torch
 from torch import optim
 import os
@@ -11,6 +12,7 @@ from torch.utils.tensorboard import SummaryWriter
 from ..utils import gen_decay_rate
 from tqdm import tqdm
 import warnings
+from typing import List
 
 class PBA_1D():
     def __init__(self, GP):
@@ -18,8 +20,8 @@ class PBA_1D():
         self.model = None
         self.N = None
         self.prop_dis = None
-        
-    def init_model(self, N, prop_dis, init_hs = None, far_field = False):
+
+    def init_model(self, N, prop_dis, init_hs=None, far_field=False):
         '''
             the layers will be used when re building the fitted model. If you change any of this default values when you do the fitting.
             you should also change at here.
@@ -30,27 +32,47 @@ class PBA_1D():
         self.far_field = far_field
 
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        if far_field:
-            self.model = PBA_model(self.prop_dis, self.GP, self.N, self.total_size, near_field = False)
+        if type(self.prop_dis) == list:
+                self.model = PBA_model_2_layer(
+                    self.prop_dis, self.GP, self.N, self.total_size, near_field=False)       
         else:
-            self.model = PBA_model(self.prop_dis, self.GP, self.N, self.total_size, near_field = True)
+            if far_field:
+                self.model = PBA_model(
+                    self.prop_dis, self.GP, self.N, self.total_size, near_field=False)
+            else:
+                self.model = PBA_model(
+                    self.prop_dis, self.GP, self.N, self.total_size, near_field=True)
         self.init_paras(self.model, init_hs)
         self.model = self.model.to(self.device)
         return None
-    
+
     def forward(self):
         '''
         output:
             if far_field == True, output is intensity otherwise is field.
         '''
         E0 = np.ones((self.total_size,))
-        E0 = torch.tensor(E0, dtype = torch.complex64)
+        E0 = torch.tensor(E0, dtype=torch.complex64)
         E0 = E0.to(self.device)
         E_out = self.model(E0)
         E_out = [E.cpu().detach().numpy() for E in E_out]
         return E_out
-    
-    def optimize(self, notes, steps, lr = 0.01, minmax = False):
+
+    def optimize(self, notes, steps, lr=0.01, minmax=True, loss_weights: List = None, target='lens', **kargs):
+        '''
+        loss_weights: we use loss_weights to compensate the intensity difference between different lam, when optimizing by minmax method.
+        '''
+        if target == 'lens':
+            deflecting_angle = None
+            pass
+
+        elif target == 'deflector':
+            assert(kargs.get("deflecting_angle")
+                   is not None), "need to specify deflecting angle."
+            assert(kargs.get("delta_degree")
+                   is not None), "need to specify the delta deflecting angle."
+            deflecting_angle = kargs['deflecting_angle']
+            delta_degree = kargs['delta_degree']
         if not self.far_field:
             raise Exception("Should initalize model with far_field=True")
         out_path = 'output_PBA/'
@@ -61,67 +83,109 @@ class PBA_1D():
         optimizer = optim.Adam(self.model.parameters(), lr=lr)
         decay_steps = steps // 10
         decay_rate = gen_decay_rate(steps, decay_steps)
-        my_lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=decay_rate)
+        my_lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(
+            optimizer, gamma=decay_rate)
         self.model.train()
         E0 = np.ones((self.total_size,))
-        E0 = torch.tensor(E0, dtype = torch.complex64)
+        E0 = torch.tensor(E0, dtype=torch.complex64)
         E0 = E0.to(self.device)
-        radius = self.N * self.GP.period/2
-        NA =  radius/ np.sqrt(radius**2 + self.prop_dis**2)
-        target_sigma = max(self.GP.lams) / (2 * NA) / self.GP.dx
-        print("the numerical aperture: ", NA, "target spot size (number of points):", target_sigma)
-        center = int(self.total_size//2)
+        if deflecting_angle is not None:
+            axis_angles = [gen_x_angle(
+                self.total_size, self.GP.dx, deflecting_angle, lam, delta_degree=delta_degree) for lam in self.GP.lams]
+        else:
+            radius = self.N * self.GP.period/2
+            if type(self.prop_dis) == list:
+                print("using the last layer prop dis to calcuate NA.")
+                NA = radius / np.sqrt(radius**2 + self.prop_dis[-1]**2)
+            else:
+                NA = radius / np.sqrt(radius**2 + self.prop_dis**2)
+            target_sigma = min(self.GP.lams) / (2 * NA) / self.GP.dx
+            print("the numerical aperture: ", NA,
+                  "target spot size (number of points):", target_sigma)
+            center = int(self.total_size//2)
+            
         for step in tqdm(range(steps + 1)):
             # Compute prediction error
-            Ifs = self.model(E0)
+            Efs = self.model(E0)
+            Ifs = [torch.abs(E)**2 for E in Efs]
+            Ef_FFTs = [torch.fft.fft(Ef) for Ef in Efs]
+            FFT_Is = [torch.abs(Ef_FFT)**2 for Ef_FFT in Ef_FFTs]
             if minmax:
-                losses = [loss_max_center(If, center, target_sigma) for If in Ifs]
+                if deflecting_angle is not None:
+                    losses = [loss_max_range(FFT_I, axis_angles[idx][1], axis_angles[idx][2])
+                              for idx, FFT_I in enumerate(FFT_Is)]
+                else:
+                    losses = [loss_max_center(If, center, target_sigma)
+                              for If in Ifs]
+                if not (loss_weights is None):
+                    for idx, w in enumerate(loss_weights):
+                        losses[idx] = losses[idx] * w
                 loss = - np.inf
                 for tmp_loss in losses:
                     if tmp_loss > loss:
                         loss = tmp_loss
             else:
                 idx = np.random.randint(0, len(self.GP.lams))
-                loss = loss_max_center(Ifs[idx], center, target_sigma)
-                
+                if deflecting_angle is not None:
+                    loss = loss_max_range(
+                        FFT_Is[idx], axis_angles[idx][1], axis_angles[idx][2])
+                else:
+                    loss = loss_max_center(Ifs[idx], center, target_sigma)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
             #grad = model.metalayer1.n_eff_paras.detach()
-            #grad_peek(grad)
+            # grad_peek(grad)
             if step % decay_steps == 0 and step != 0:
                 my_lr_scheduler.step()
-                
+
             if step % decay_steps == 0:    # every 1000 mini-batches...
 
                 # ...log the running loss
                 writer.add_scalar('training loss',
-                                scalar_value = loss.item(), global_step = step)
-                writer.add_figure('hs',
-                                plot_hs(self.model.hs.cpu().detach().numpy()),
-                                global_step= step)
-                for i, If in enumerate(Ifs):
-                    writer.add_figure(f"If, lam: {self.GP.lams[i]} um",
-                                    plot_If(If),
-                                    global_step= step)       
+                                  scalar_value=loss.item(), global_step=step)
+                if type(self.prop_dis) == list:
+                    for idx in range(len(self.prop_dis)):
+                        writer.add_figure('hs_layer_' + str(idx),
+                                        plot_hs(
+                                            self.model.PBA_models[idx].hs.cpu().detach().numpy()),
+                                        global_step=step)   
+                else:   
+                    writer.add_figure('hs',
+                                    plot_hs(
+                                        self.model.hs.cpu().detach().numpy()),
+                                    global_step=step)
+                for idx, If in enumerate(Ifs):
+                    out_If = If.cpu().detach().numpy()
+                    writer.add_figure(f"If, lam: {self.GP.lams[idx]} um",
+                                      plot_If(out_If),
+                                      global_step=step)
+                    if deflecting_angle is not None:
+                        writer.add_figure(f"abs(FFT of Ef)**2, lam: {self.GP.lams[idx]} um",
+                                          plot_FFT_I(
+                                              axis_angles[idx][0], axis_angles[idx][1], axis_angles[idx][2], FFT_Is[idx]),
+                                          global_step=step)
 
                 # loss = loss.item()
                 # loss_list.append(loss)
                 # print(f"loss: {loss:>7f}  [{step:>5d}/{train_steps:>5d}]")
         print("final lr:", my_lr_scheduler.get_last_lr())
         out_pos = (np.arange(self.N) - (self.N - 1)/2) * self.GP.period
-        out_hs = self.model.hs.cpu().detach().numpy()
-        out_data = np.c_[out_pos.reshape(-1,1), out_hs.reshape(-1, 1)]
-        np.savetxt(out_path + 'waveguide_widths.csv', out_data, delimiter=",")
+        if type(self.prop_dis) == list:
+            for idx in range(len(self.prop_dis)):
+                out_hs = self.model.PBA_models[idx].hs.cpu().detach().numpy()
+                out_data = np.c_[out_pos.reshape(-1, 1), out_hs.reshape(-1, 1)]
+                np.savetxt(out_path + f'waveguide_widths_{idx}.csv', out_data, delimiter=",") 
+        else:   
+            out_hs = self.model.hs.cpu().detach().numpy()
+            out_data = np.c_[out_pos.reshape(-1, 1), out_hs.reshape(-1, 1)]
+            np.savetxt(out_path + 'waveguide_widths.csv', out_data, delimiter=",")
         print('parameters saved in.', out_path)
         return None
 
-    def init_paras(self, model, init_hs = None):
-        model.reset()
-        if init_hs is None:
-            print('initialized by default h_paras.')
-            return None
-        else:
+    def init_paras(self, model, init_hs=None):
+        
+        def hs_preprocess(init_hs):
             #h_paras = np.genfromtxt(path, delimiter=',')
             if init_hs.max() > self.GP.h_max:
                 warnings.warn("bad initial widths for waveguides.")
@@ -129,25 +193,41 @@ class PBA_1D():
             if init_hs.min() < self.GP.h_min:
                 warnings.warn("bad initial widths for waveguides.")
                 print("initial widths smaller than h_min, replaced by h_min")
-            hs_paras = (init_hs - self.GP.h_min)/ (self.GP.h_max - self.GP.h_min)
+            hs_paras = (init_hs - self.GP.h_min) / \
+                (self.GP.h_max - self.GP.h_min)
             hs_paras = np.minimum(np.maximum(hs_paras, 0.01), 0.99)
-            #becuase in our model we use Sigmoid  function, here, the hs paras is generated by inverse function.
-            init_hs_para = np.log(hs_paras / (1 - hs_paras)) 
-            init_hs_para = torch.tensor(init_hs_para, dtype = torch.float)
-            state_dict = model.state_dict()
+            # becuase in our model we use Sigmoid  function, here, the hs paras is generated by inverse function.
+            init_hs_para = np.log(hs_paras / (1 - hs_paras))
+            init_hs_para = torch.tensor(init_hs_para, dtype=torch.float)
+            return init_hs_para
+        
+        model.reset()
+        if init_hs is None:
+            print('initialized by default h_paras.')
+            return None
+        state_dict = model.state_dict()
+        if type(init_hs) == list:
+            for idx, tmp_init_hs in enumerate(init_hs):
+                if tmp_init_hs is None:
+                    continue
+                state_dict['PBA_models.' + str(idx) + '.h_paras'] = hs_preprocess(tmp_init_hs)
+        else:
+            init_hs_para = hs_preprocess(init_hs)
             state_dict['h_paras'] = init_hs_para
-            model.load_state_dict(state_dict)
-            #with torch.no_grad():
-            #    model.metalayer1.h_paras.data = h_paras_initial
-            print('initialized by loaded h_paras.')
-            return None 
+            
+        model.load_state_dict(state_dict)
+        # with torch.no_grad():
+        #    model.metalayer1.h_paras.data = h_paras_initial
+        print('initialized by loaded h_paras.')
+        return None
+
     def vis_field(self, E):
         px = (np.arange(self.total_size) - self.total_size//2) * self.GP.dx
         if self.far_field:
             for i, lam in enumerate(self.GP.lams):
                 Ii = E[i]
                 plt.figure()
-                plt.plot(px, Ii, label = 'intensity')
+                plt.plot(px, Ii, label='intensity')
                 plt.legend()
                 plt.xlabel("postion [um]")
                 plt.title("wavelength: " + str(lam))
@@ -155,38 +235,17 @@ class PBA_1D():
         else:
             for i, lam in enumerate(self.GP.lams):
                 Ei = E[i]
-                fig, axs = plt.subplots(1, 2, figsize = (12, 6))
-                _ = axs[0].plot(px, np.angle(Ei), label = "phase")
+                fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+                _ = axs[0].plot(px, np.angle(Ei), label="phase")
                 axs[0].legend()
                 axs[0].set_xlabel("postion [um]")
-                _ = axs[1].plot(px, np.abs(Ei), label = "amp")
+                _ = axs[1].plot(px, np.abs(Ei), label="amp")
                 axs[1].legend()
                 axs[1].set_xlabel("postion [um]")
                 plt.title("wavelength: " + str(lam))
                 plt.show()
         return None
 
-def plot_hs(out_hs):
-    fig = plt.figure()
-    plt.plot(out_hs)
-    return fig
-
-def plot_If(If, target_If = None):
-    out_If = If.cpu().detach().numpy()
-    fig, axs = plt.subplots(1, 1, figsize = (8, 6))
-    plt.ioff()
-    axs.plot(out_If, label = 'output')
-    #print('sum of intensity:', out_If.sum())
-    if not (target_If is None):
-        target_If = target_If * out_If.max()
-        axs.plot(target_If, label = 'target normalized by max(out_If)')
-    axs.legend()
-    #plt.ylabel('intensity normalized by max')
-    return fig
-
-def loss_max_center(If, center, max_length):
-    intensity = torch.sum(torch.abs(If[center - int(max_length//2): center + int(max_length//2)]))
-    return - intensity
 
 def gaussian_func(x, mu, sigma):
     return np.exp(- (x - mu)**2 / (2 * sigma**2))

Meta_SCMT/PBA_utils/PBA_design.py
@@ -60,7 +60,7 @@ class PBA():
                             pml_layers=pml_layers)
         return sim
 
-    def create_sim(self, width):   
+    def create_sim(self, width, inverse):   
         import grcwa
         #grcwa.set_backend('autograd')  # important!!
         grcwa.set_backend('numpy')
@@ -111,10 +111,16 @@ class PBA():
         ######### setting up RCWA
         obj = grcwa.obj(nG,L1,L2,freqcmp,theta,phi,verbose=0)
         # input layer information
-        obj.Add_LayerUniform(thick0,ep0)
-        obj.Add_LayerGrid(thickp,Nx,Ny)
-        obj.Add_LayerGrid(wavelength,Nx,Ny)
-        obj.Add_LayerUniform(thickN,epN)
+        if inverse:
+            obj.Add_LayerUniform(thickN,epN)
+            obj.Add_LayerGrid(thickp,Nx,Ny)
+            obj.Add_LayerGrid(wavelength,Nx,Ny)
+            obj.Add_LayerUniform(thick0,ep0)
+        else: 
+            obj.Add_LayerUniform(thick0,ep0)
+            obj.Add_LayerGrid(thickp,Nx,Ny)
+            obj.Add_LayerGrid(wavelength,Nx,Ny)
+            obj.Add_LayerUniform(thickN,epN)
         obj.Init_Setup()
         obj.MakeExcitationPlanewave(planewave['p_amp'],planewave['p_phase'],planewave['s_amp'],planewave['s_phase'],order = 0)    
         epgrid = np.concatenate((pillar_eps.flatten(), air_eps.flatten()))
@@ -123,7 +129,11 @@ class PBA():
         field = obj.Solve_FieldOnGrid(2,z_offset = wavelength)
         return obj, pillar_eps.reshape(Ny, Nx), field
 
-    def gen_lib(self, vis = True, backend = 'grcwa', step_size = 0.001):
+    def gen_lib(self, vis = True, backend = 'grcwa', step_size = 0.001, inverse = False):
+        '''
+            inverse: if inverse is true, the light is incident from air to waveguide then to substrate, else: the light is from substrate to waveguide then  to air.
+        
+        '''
         widths = np.arange(self.GP.h_min, self.GP.h_max + step_size, step_size)
         phases = []
         if backend == 'tidy3d':
@@ -146,7 +156,7 @@ class PBA():
                 phases.append(get_phase_tidy3d(sim))   
         elif backend == 'grcwa':
             for w in tqdm(widths):
-                _, _, field = self.create_sim(w)
+                _, _, field = self.create_sim(w, inverse)
                 ph = get_phase(field)
                 phases.append(ph)
         phases = np.array(phases)
@@ -160,30 +170,43 @@ class PBA():
             #plt.plot(widths_finer, phases_finer)
             plt.xlabel("Waveguide width [um]")
             plt.ylabel("Phase")
-            plt.savefig(self.GP.path + "PBA_phase_vs_width.png")
+            if inverse:
+                plt.savefig(self.GP.path + "PBA_phase_vs_width_inverse.png")
+            else:
+                plt.savefig(self.GP.path + "PBA_phase_vs_width.png")
             plt.show()
         L = phases.shape[0]
         width_phase_map = np.zeros((2, L))
         width_phase_map[0] = widths
         width_phase_map[1] = phases
-        np.save(self.GP.path + "rcwa_width_phase_map.npy", width_phase_map)
+        if inverse:
+            np.save(self.GP.path + "rcwa_width_phase_map_inverse.npy", width_phase_map)
+        else:
+            np.save(self.GP.path + "rcwa_width_phase_map.npy", width_phase_map)
         print("PBA width phase map saved.")
         self.width_phase_map = width_phase_map
         return None
 
-    def fit(self, layers = 6, nodes = 64, steps = 1000, lr = 0.001, vis = True, load = True):
+    def fit(self, layers = 6, nodes = 64, steps = 1000, lr = 0.001, vis = True, load = True, inverse = False):
         if load:
-            self.width_phase_map = np.load(self.GP.path + "rcwa_width_phase_map.npy")
+            if inverse:
+                self.width_phase_map = np.load(self.GP.path + "rcwa_width_phase_map_inverse.npy")
+            else:
+                self.width_phase_map = np.load(self.GP.path + "rcwa_width_phase_map.npy")
         else:
             if self.width_phase_map is None:
-                self.gen_lib()
+                self.gen_lib(inverse=inverse)
         X, Y = self.width_phase_map[0], self.width_phase_map[1]
         X = X.reshape(-1, 1)
         Y = Y.reshape(-1, 1)
         self.model = Model(1, 1, layers= layers, nodes = nodes)
         batch_size = 512
         Y_pred = train(self.model, X, Y, steps, lr, batch_size)
-        torch.save(self.model.state_dict(), self.GP.path + "fitting_PBA_state_dict")
+        if inverse:
+            torch.save(self.model.state_dict(), self.GP.path + "fitting_PBA_state_dict_inverse")
+        else:
+            torch.save(self.model.state_dict(), self.GP.path + "fitting_PBA_state_dict")
+            
         paras = {'nodes': nodes, 'layers': layers}
         np.save(self.GP.path + "PBA_paras.npy", paras)
         print("model saved.")
@@ -326,7 +349,8 @@ def gen_width_from_phase(width_phase_map, target_phase_profile):
     phases = phases.reshape(1,-1)
     target_shape = target_phase_profile.shape
     target_phase_profile = target_phase_profile.reshape(-1,1)
-    diff = np.abs(target_phase_profile - phases)
+    #diff = np.abs(target_phase_profile - phases)
+    diff = np.abs(np.exp(1j*target_phase_profile) - np.exp(1j * phases))
     indexes = np.argmin(diff, axis = -1)
     widths_map = np.take(widths, indexes)
     widths_map = widths_map.reshape(target_shape)

Meta_SCMT/PBA_utils/PBA_design_lam-LAPTOP-ND4QN1I7.py
@@ -16,13 +16,14 @@ class PBA():
     def __init__(self, GP, dim):
         self.GP = GP
         self.dim = dim  # dim = 1 or 2.
-        self.width_phase_maps = []
+        self.width_phase_maps = None
         self.model = None
 
     # TODO: modify design lens for multiple lam.
     def design_lens(self, N, focal_length, vis=True, quarter=False):
         designed_phases = []
         designed_lenses = []
+        self.width_phase_maps = []
         for idx, lam in enumerate(self.GP.lams):
             self.width_phase_maps.append(
                 np.load(self.GP.paths[idx] + "rcwa_width_phase_map.npy"))
@@ -86,6 +87,7 @@ class PBA():
     def design_deflector(self, N, degree, vis=True):
         designed_phases = []
         designed_lenses = []
+        self.width_phase_maps = []
         for idx, lam in enumerate(self.GP.lams):
             self.width_phase_maps.append(
                 np.load(self.GP.paths[idx] + "rcwa_width_phase_map.npy"))
@@ -153,7 +155,7 @@ def gen_width_from_phase(width_phase_maps: List, target_phases: List):
     phases = phases.T
     phases = phases.reshape(1, -1, num_phases)
     target_phases = target_phases.reshape(-1, 1, num_phases)
-    diff = np.sum((np.abs(np.exp(1j * target_phases) - np.exp(1j * phases)))**2, axis=-1)
+    diff = np.sum((target_phases - phases)**2, axis=-1)
     indexes = np.argmin(diff, axis=-1)
     widths_map = np.take(widths, indexes)
     widths_map = widths_map.reshape(target_shape)

Meta_SCMT/SCMT_utils/SCMT_1D_lam.py
@@ -102,7 +102,7 @@ class SCMT_1D():
         else:
             radius = self.N * self.GP.period/2
             NA = radius / np.sqrt(radius**2 + self.prop_dis**2)
-            target_sigma = self.GP.lam / (2 * NA) / self.GP.dx
+            target_sigma = min(self.GP.lams) / (2 * NA) / self.GP.dx
             print("the numerical aperture: ", NA,
                   "target spot size (number of points):", target_sigma)
             center = int(self.total_size//2)
@@ -112,7 +112,7 @@ class SCMT_1D():
             Efs = self.model(E0)
             Ifs = [torch.abs(E)**2 for E in Efs]
             Ef_FFTs = [torch.fft.fft(Ef) for Ef in Efs]
-            FFT_Is = [torch.abs(Ef_FFT) for Ef_FFT in Ef_FFTs]
+            FFT_Is = [torch.abs(Ef_FFT)**2 for Ef_FFT in Ef_FFTs]
             if minmax:
                 if deflecting_angle is not None:
                     losses = [loss_max_range(FFT_I, axis_angles[idx][1], axis_angles[idx][2])
@@ -158,7 +158,7 @@ class SCMT_1D():
                                       plot_If(out_If),
                                       global_step=step)
                     if deflecting_angle is not None:
-                        writer.add_figure(f"abs(FFT of Ef), lam: {self.GP.lams[idx]} um",
+                        writer.add_figure(f"abs(FFT of Ef)**2, lam: {self.GP.lams[idx]} um",
                                           plot_FFT_I(
                                               axis_angles[idx][0], axis_angles[idx][1], axis_angles[idx][2], FFT_Is[idx]),
                                           global_step=step)

Meta_SCMT/fullwave_1D_2layer.py
@@ -9,12 +9,12 @@ from .ideal_meta_1D import Ideal_meta
 from typing import Optional, Dict, Tuple, Union, List
 
 
-class Fullwave_1D():
+class Fullwave_1D_2layer():
     def __init__(self, GP) -> None:
         self.GP = GP
         self.sim = None
 
-    def init_sim(self, prop_dis: float, N: int, hs: np.ndarray, res: Optional[int] = None, theta: float = 0, empty: bool = False, backend: str = 'meep', vis_path: Optional[str] = None) -> None:
+    def init_sim(self, prop_dis: Union[float, List[float]], N: int, hs: Union[np.ndarray, List[np.ndarray]], res: Optional[int] = None, theta: float = 0, empty: bool = False, backend: str = 'meep', vis_path: Optional[str] = None) -> None:
         self.backend = backend
         self.vis_path = vis_path
         if backend == 'meep':
@@ -197,17 +197,10 @@ class Fullwave_1D():
             plt.savefig(self.vis_path + "near_and_far_field.png")
         return ez_data, data_in, data_near, data_far
 
-    def tidy3d_init_sim(self, prop_dis: float, N: int, hs: np.ndarray, res: Optional[int] = None, theta: float = 0, empty: bool = False) -> None:
+    def tidy3d_init_sim(self, prop_dis: Union[float, List[float]], N: int, hs:  Union[np.ndarray, List[np.ndarray]], res: Optional[int] = None, theta: float = 0, empty: bool = False) -> None:
         # tidy3D import
         import tidy3d as td
-        warnings.warn(
-            "Fullwave is expensive and slow. Only do fullwave on small devices. And low resolution can be inaccurate.")
-        if hs.max() > self.GP.h_max:
-            warnings.warn(
-                "initial widths larger than h_max, bad initial widths for waveguides.")
-        if hs.min() < self.GP.h_min:
-            warnings.warn(
-                "initial widths smaller than h_min, bad initial widths for waveguides.")
+
         if res == None:
             self.res = int(round(1 / self.GP.dh))
         else:
@@ -217,12 +210,12 @@ class Fullwave_1D():
         self.N = N
         self.prop_dis = prop_dis
         xh = N * self.GP.period
-        NA = np.sin(xh / np.sqrt(xh**2 + prop_dis**2))
+        NA = np.sin(xh / np.sqrt(xh**2 + self.prop_dis[-1]**2))
         print(f"numerical aperture: {NA:.2f}")
         self.min_focal_spot = self.GP.lam / 2 / NA
         self.efficiency_length = 6 * self.min_focal_spot
         # Simulation domain size (in micron)
-        z_size = self.GP.wh + 2 + prop_dis
+        z_size = 2 * self.GP.wh + 2 + prop_dis[0] + prop_dis[1]
         x_wgs = N * self.GP.period
         #x_aper = N * self.GP.period
         x_aper = 0
@@ -246,8 +239,8 @@ class Fullwave_1D():
         positions = []
         if self.GP.n_sub != 1:
             material2 = td.Medium(epsilon=self.GP.n_sub**2)
-            sub = td.Box(center=[0, 0, -z_size/2],
-                         size=[x_size*2, y_size*2, 2], material=material2)
+            sub = td.Box(center=[0, 0, z_plane + self.GP.wh + prop_dis[0]/2],
+                         size=[x_size*2, y_size*2, prop_dis[0]], material=material2)
             waveguides.append(sub)
         # gold = td.material_library.Au()
         # aper1 = td.Box(center=[-(x_aper + x_wgs)/2 - x_aper, 0, z_plane + self.GP.wh/2], size=[2 * x_aper, y_size*2, self.GP.wh], material=gold)
@@ -256,19 +249,22 @@ class Fullwave_1D():
         # waveguides.append(aper2)
         if not empty:
             for i in range(N):
-                width = hs[i]
+                width = hs[0][i]
                 x = X[i]
                 positions.append(float(x))
                 temp_wg = td.Box(center=[x, 0, z_plane + self.GP.wh/2],
                                  size=[width, y_size*2, self.GP.wh], material=material1)
                 waveguides.append(temp_wg)
-                # temp_wg = td.Box(center=[x - x_wgs, 0, z_plane + self.GP.wh/2], size=[width, y_size*2, self.GP.wh], material=material1)
-                # waveguides.append(temp_wg)
-                # temp_wg = td.Box(center=[x + x_wgs, 0, z_plane + self.GP.wh/2], size=[width, y_size*2, self.GP.wh], material=material1)
-                # waveguides.append(temp_wg)
+                width = hs[1][i]
+                x = X[i]
+                #positions.append(float(x))
+                temp_wg = td.Box(center=[x, 0, z_plane + 3*self.GP.wh/2 + self.prop_dis[0]],
+                                 size=[width, y_size*2, self.GP.wh], material=material1)
+                waveguides.append(temp_wg)
+
 
         positions = np.array(positions)
-        self.hs_with_pos = np.c_[hs.reshape((-1, 1)), positions]
+        self.hs_with_pos = np.c_[hs[0].reshape((-1, 1)), positions]
 
         # psource = td.PlaneWave(
         #     injection_axis='+z',
@@ -292,12 +288,12 @@ class Fullwave_1D():
                                     x_size, 0, z_size], freqs=[fcen])
         # focal plane monitor.
         monitor_far = td.FreqMonitor(
-            center=[0, 0, z_plane + self.GP.wh + prop_dis], size=[x_wgs, y_size, 0], freqs=[fcen])
+            center=[0, 0, z_plane + 2 * self.GP.wh + prop_dis[0] + prop_dis[1]], size=[x_wgs, y_size, 0], freqs=[fcen])
         monitor_near = td.FreqMonitor(center=[
-                                      0, 0, z_plane + self.GP.wh + self.GP.lam/2], size=[x_wgs, y_size, 0], freqs=[fcen])
+                                      0, 0, z_plane + 2 * self.GP.wh + prop_dis[0] + self.GP.lam/2], size=[x_wgs, y_size, 0], freqs=[fcen])
         monitor_in = td.FreqMonitor(
             center=[0, 0, -z_size/2 + 0.7], size=[x_wgs, y_size, 0], freqs=[fcen])
-        monitor_focus = td.FreqMonitor(center=[0, 0, z_plane + self.GP.wh + prop_dis], size=[
+        monitor_focus = td.FreqMonitor(center=[0, 0, z_plane+ 2 * self.GP.wh + prop_dis[0] + prop_dis[1]], size=[
                                        self.efficiency_length, y_size, 0], freqs=[fcen])
         monitor_back = td.FreqMonitor(
             center=[0, 0, -z_size/2 + 0.2], size=[x_wgs, y_size, 0], freqs=[fcen])
@@ -368,8 +364,8 @@ class Fullwave_1D():
         # Ey_xz_raw = Ey_xz_raw[:, c - r: c + r]
         phy_size_x = Ey_xz_raw.shape[1] * step1
         phy_size_y = Ey_xz_raw.shape[0] * step1
-        index_near = int(round((1 + self.GP.wh)/step1))
-        index_far = int(round((1 + self.GP.wh + self.prop_dis)/step1))
+        index_near = int(round((1 + 2 *self.GP.wh + self.prop_dis[0])/step1))
+        index_far = int(round((1 + 2*self.GP.wh + self.prop_dis[0] + self.prop_dis[1])/step1))
         index_in = int(round((0.2)/step1))
         Ey_near = Ey_xz_raw[index_near, :]
         Ey_far = Ey_xz_raw[index_far, :]
@@ -438,7 +434,7 @@ class Fullwave_1D():
         fwhm = FWHM(xs_far, I_far)
         print(f'fwhm = {fwhm:.4f} um, {(fwhm / self.GP.lam):.2f} $\lambda$')
         ideal_meta = Ideal_meta(self.GP)
-        ideal_meta.model_init(self.N, self.prop_dis, lens=True)
+        ideal_meta.model_init(self.N, self.prop_dis[1], lens=True)
         xs_ideal, I_ideal = ideal_meta.forward(vis=False)
         I_ideal = np.interp(xs_far, xs_ideal, I_ideal)
         power_ideal = np.sum(I_ideal)